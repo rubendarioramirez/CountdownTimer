c5a85902181770e59cc2032cc8239ebf32960f99
Ruben Ramirez <ramirez.ruben.dario10@gmail.com>
2016-09-05 17:46:26 +0700

    -lastcommit

--- app/src/main/java/com/android/rramirez/countdowntimer/timerWidget.java
+++ app/src/main/java/com/android/rramirez/countdowntimer/timerWidget.java
@@ -1,7 +1,5 @@
 package com.android.rramirez.countdowntimer;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.BroadcastReceiver;
@@ -10,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.widget.RemoteViews;
 import java.text.ParseException;
@@ -19,54 +16,10 @@ import java.util.Date;
 
 public class timerWidget extends AppWidgetProvider {
 
-    private static int WIDGET_REFRESH_TIME = 1;
-    public Handler handler = new Handler();
+
       public Intent intent;
       private Context ctx;
 
-//    Runnable updateWidgetText = new Runnable()
-//    {
-//        @Override
-//        public void run() {
-////            AppWidgetManager gm = AppWidgetManager.getInstance(ctx);
-////            ComponentName thisWidget = new ComponentName(ctx, timerWidget.class);
-////
-////            try{
-////                //Fetch fecha from SharedPref
-////                String fetchFecha = Utility.getString(ctx, "fecha", "");
-////                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-////                // Here Set your Event Date
-////                Date futureDate = dateFormat.parse(fetchFecha);
-////                Date currentDate = new Date();
-////                if (!currentDate.after(futureDate)) {
-////                    long diff = futureDate.getTime()
-////                            - currentDate.getTime();
-////                    long days = diff / (24 * 60 * 60 * 1000);
-////                    diff -= days * (24 * 60 * 60 * 1000);
-////                    long hours = diff / (60 * 60 * 1000);
-////                    diff -= hours * (60 * 60 * 1000);
-////                    long minutes = diff / (60 * 1000);
-////                    diff -= minutes * (60 * 1000);
-////                    long seconds = diff / 1000;
-////
-////                    RemoteViews views = new RemoteViews(ctx.getPackageName(), R.layout.timer_widget);
-////                    views.setTextViewText(R.id.dayCount,  String.format("%d", days));
-////                    views.setTextViewText(R.id.hourCount, String.format("%d", hours));
-////                    views.setTextViewText(R.id.minCount, String.format("%d", minutes));
-////                    views.setTextViewText(R.id.secondCount, String.format("%d", seconds));
-////
-////                    gm.updateAppWidget(thisWidget, views);
-////
-////                }
-////
-////            } catch (ParseException e) {
-////                e.printStackTrace();
-////            }
-////
-////            handler.postDelayed(this, WIDGET_REFRESH_TIME * 1000);
-////
-//        }
-//    };
 
     //Gets the updates for the CountDown.
     private BroadcastReceiver br = new BroadcastReceiver() {
@@ -74,6 +27,7 @@ public class timerWidget extends AppWidgetProvider {
         public void onReceive(Context context, Intent intent) {
             try {
                 updateGUI(intent); // or whatever method used to update your GUI fields
+
             } catch (ParseException e) {
                 e.printStackTrace();
             }
@@ -81,53 +35,53 @@ public class timerWidget extends AppWidgetProvider {
     };
 
     private void updateGUI(Intent intent) throws ParseException {
+        Log.i("CONTEXT 1", TimerService.context.toString());
         if (intent.getExtras() != null) {
 
-            Utility.makeToast(ctx, "getting GUI");
-            //Fetch fecha from SharedPref
-            String fetchFecha = Utility.getString(ctx, "fecha", "");
-            Log.d("app", "dummy");
+          //  Utility.makeToast(TimerService.context, "getting GUI");
+            Log.i("CONTEXT 2", TimerService.context.toString());
 
-            AppWidgetManager gm = AppWidgetManager.getInstance(ctx);
-            ComponentName thisWidget = new ComponentName(ctx, timerWidget.class);
+            AppWidgetManager gm = AppWidgetManager.getInstance(TimerService.context);
+            ComponentName thisWidget = new ComponentName(TimerService.context, timerWidget.class);
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            // Here Set your Event Date
+            //Fetch fecha from SharedPref
+            String fetchFecha = Utility.getString(TimerService.context, "fecha", "");
             Date futureDate = dateFormat.parse(fetchFecha);
             Date currentDate = new Date();
             if (!currentDate.after(futureDate)) {
                 long diff = futureDate.getTime() - currentDate.getTime();
-                    long days = diff / (24 * 60 * 60 * 1000);
-                    diff -= days * (24 * 60 * 60 * 1000);
-                    long hours = diff / (60 * 60 * 1000);
-                    diff -= hours * (60 * 60 * 1000);
-                    long minutes = diff / (60 * 1000);
-                    diff -= minutes * (60 * 1000);
-                    long seconds = diff / 1000;
+                long days = diff / (24 * 60 * 60 * 1000);
+                diff -= days * (24 * 60 * 60 * 1000);
+                long hours = diff / (60 * 60 * 1000);
+                diff -= hours * (60 * 60 * 1000);
+                long minutes = diff / (60 * 1000);
+                diff -= minutes * (60 * 1000);
+                long seconds = diff / 1000;
 
-                    RemoteViews views = new RemoteViews(ctx.getPackageName(), R.layout.timer_widget);
-                    views.setTextViewText(R.id.dayCount,  String.format("%d", days));
-                    views.setTextViewText(R.id.hourCount, String.format("%d", hours));
-                    views.setTextViewText(R.id.minCount, String.format("%d", minutes));
-                    views.setTextViewText(R.id.secondCount, String.format("%d", seconds));
+                RemoteViews views = new RemoteViews(TimerService.context.getPackageName(), R.layout.timer_widget);
+                views.setTextViewText(R.id.dayCount, String.format("%d", days));
+                views.setTextViewText(R.id.hourCount, String.format("%d", hours));
+                views.setTextViewText(R.id.minCount, String.format("%d", minutes));
+                views.setTextViewText(R.id.secondCount, String.format("%d", seconds));
 
-                    gm.updateAppWidget(thisWidget, views);
-
-                }
+                gm.updateAppWidget(thisWidget, views);
 
             }
+
         }
+    }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        super.onUpdate(TimerService.context, appWidgetManager, appWidgetIds);
 
-        String title1 = Utility.getString(context, "title1", "");
-        String title2 = Utility.getString(context, "title2", "");
-        String text1 = Utility.getString(context, "text1", "");
+        String title1 = Utility.getString(TimerService.context, "title1", "");
+        String title2 = Utility.getString(TimerService.context, "title2", "");
+        String text1 = Utility.getString(TimerService.context, "text1", "");
 
-        ComponentName thisWidget = new ComponentName(context, timerWidget.class);
+        ComponentName thisWidget = new ComponentName(TimerService.context, timerWidget.class);
         for (int widgetId : appWidgetManager.getAppWidgetIds(thisWidget)) {
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.timer_widget);
+            RemoteViews views = new RemoteViews(TimerService.context.getPackageName(), R.layout.timer_widget);
 
             views.setTextViewText(R.id.tvTitle1, title1);
             views.setTextViewText(R.id.tvTitle2, title2);
@@ -139,17 +93,11 @@ public class timerWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-       super.onEnabled(context);
-        intent = new Intent(context, TimerService.class);
-
-//        AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-//        PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
-//        //After 3 seconds
-//        am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000, pi);
+       super.onEnabled(TimerService.context);
+        intent = new Intent(TimerService.context, TimerService.class);
 
         try {
-            context.getApplicationContext().registerReceiver(br, new IntentFilter(TimerService.COUNTDOWN_BR));} catch (Exception e) {
+            TimerService.context.registerReceiver(br, new IntentFilter(TimerService.COUNTDOWN_BR));} catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -159,39 +107,24 @@ public class timerWidget extends AppWidgetProvider {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        super.onReceive(context, intent);
-
-        ctx = context;
-
-//        if (intent.getAction().equals("android.appwidget.action.APPWIDGET_ENABLED")) {
-//            handler.removeCallbacks(updateWidgetText);
-//            ctx = context;
-//            handler.postDelayed(updateWidgetText, WIDGET_REFRESH_TIME*1000);
-//        }
-//        else if (intent.getAction().equals("android.appwidget.action.APPWIDGET_DISABLED")) {
-//            handler.removeCallbacks(updateWidgetText);
-//        }
+        super.onReceive(TimerService.context, intent);
 
 
     }
 
     @Override
     public void onDisabled(Context context) {
-//        Intent intent = new Intent(context, AlarmManagerBroadcastReceiver.class);
-//        PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
-//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-//        alarmManager.cancel(sender);
-        super.onDisabled(context);
+        super.onDisabled(TimerService.context);
     }
 
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
-        super.onDeleted(context, appWidgetIds);
+        super.onDeleted(TimerService.context, appWidgetIds);
     }
 
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
-        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
+        super.onAppWidgetOptionsChanged(TimerService.context, appWidgetManager, appWidgetId, newOptions);
 
     }
 
