38ca2f5d1ad8ad13bb26df3c8b206f42f71b7b7b
Ruben Ramirez <ramirez.ruben.dario10@gmail.com>
2018-10-23 11:19:03 +0700

    Updated all SDKs and implementations.

--- app/src/main/java/com/android/rramirez/countdowntimer/Utility.java
+++ app/src/main/java/com/android/rramirez/countdowntimer/Utility.java
@@ -1,20 +1,9 @@
 package com.android.rramirez.countdowntimer;
 
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
-import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
